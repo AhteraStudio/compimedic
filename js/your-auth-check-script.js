5bee7b37653e2cdd0b7432685f6cf0025d445a4d
Ahtera Studio <eadlalamsa@gmail.com>
2024-08-20 17:10:38 +0700

    Create your-auth-check-script.js

---
+++ js/your-auth-check-script.js
@@ -0,0 +1,6 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (!user) {
+        // User is not logged in, redirect to login page
+        window.location.href = 'login.html';
+    }
+});
